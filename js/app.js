3dce395ea23de5c9731fd705e76f5a2afbaee807
Zachar13y <zachar13y@gmail.com>
2018-08-21 19:24:08 -0700

    Updated README, html layout complete, hooked in style.css, created images array in app.js.

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var images = []; 
+    ('bag.jpg');
+    ('banana.jpg');
+    ('boots.jpg');
+    ('chair.jpg');
+    ('cthulhu.jpg');
+    ('dragon.jpg');
+    ('pen.jpg');
+    ('scissors.jpg');
+    ('shark.jpg');
+    ('sweep.jpg');
+    ('unicorn.jpg');
+    ('usb.jpg');
+    ('water_can.jpg');
+    ('wine_glass.jpg');
+
+    function showThreeImages() {
+        
+    }
