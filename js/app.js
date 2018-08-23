f4782e5b2a2f5bba9805bca1a0885e2aa6c3ca52
Zachar13y <zachar13y@gmail.com>
2018-08-22 19:41:31 -0700

    Pushed the three printed images into an array.

--- js/app.js
+++ js/app.js
@@ -18,32 +18,45 @@ images.push(new BusMallImage('usb.jpg'));
 images.push(new BusMallImage('water_can.jpg'));
 images.push(new BusMallImage('wine_glass.jpg'));
 
-    console.log(images);
+console.log(images);
+
+
 
 function showThreeImages() {
     var container = document.getElementById("img-container");
     container.innerText = '';
     var image = document.createElement('img');
+    var printedPics = [];
     var index = generateRandom(0, images.length);
     image.setAttribute('src', './img/' + images[index].fileName);
     image.addEventListener('click', clickedPic);
     container.appendChild(image);
+    printedPics.push(image);
+
+
     image = document.createElement('img');
     index = generateRandom(0, images.length);
     image.setAttribute('src', './img/' + images[index].fileName);
     image.addEventListener('click', clickedPic);
     container.appendChild(image);
+    printedPics.push(image);
+
+
     image = document.createElement('img');
     index = generateRandom(0, images.length);
     image.setAttribute('src', './img/' + images[index].fileName);
     image.addEventListener('click', clickedPic)
     container.appendChild(image);
+    printedPics.push(image);
     
 
-    container.appendChild(image);
-    console.log(index);
-    // console.log(showThreeImages(index));
+    console.log(printedPics);
+    console.log(printedPics[0].currentSrc);
+    console.log(printedPics[0].src);
+
 }
+
+
 
 function generateRandom(min, max) {
     return Math.floor((Math.random() * (max - min)) + min);
@@ -54,4 +67,3 @@ function clickedPic(event) {
     }
 
     window.addEventListener('load', showThreeImages);
-
