2f0a145e600cf8743578f29893f1e31ec2c3e238
Zachar13y <zachar13y@gmail.com>
2018-08-23 19:49:27 -0700

    Cleaned up code, synching master.

--- js/app.js
+++ js/app.js
@@ -18,31 +18,28 @@ images.push(new BusMallImage('cthulhu.jpg'));
 // images.push(new BusMallImage('water_can.jpg'));
 // images.push(new BusMallImage('wine_glass.jpg'));
 
-// console.log(images);
-
-
 
 function showThreeImages() {
     var container = document.getElementById("img-container");
     container.innerText = '';
-    var image = document.createElement('img');
+    var image1 = document.createElement('img');
     var printedPics = [];
     var index = generateRandom(0, images.length);
-    image.setAttribute('src', './img/' + images[index].fileName);
-    image.addEventListener('click', clickedPic);
-    container.appendChild(image);
-    printedPics.push(image);
+    image1.setAttribute('src', './img/' + images[index].fileName);
+    image1.addEventListener('click', clickedPic);
+    container.appendChild(image1);
+    printedPics.push(image1);
 
 
     var image2 = document.createElement('img');
     index = generateRandom(0, images.length);
     image2.setAttribute('src', './img/' + images[index].fileName);
 
-    while (image.src === image2.src) {   //////////While loop that compares the first two images//////////////
+    while (image1.src === image2.src) {   //////////While loop that compares the first two images//////////////
         console.log('Images 1 and 2 were the same');
         var changeIndex = generateRandom(0, images.length);
         image2.src = ('./img/' + images[changeIndex].fileName);
-    }
+    };
 
     image2.addEventListener('click', clickedPic);
     container.appendChild(image2);
@@ -53,36 +50,25 @@ function showThreeImages() {
     index = generateRandom(0, images.length);
     image3.setAttribute('src', './img/' + images[index].fileName);
 
-    while (image2.src === image3.src) {
+    while (image2.src === image3.src) { //////////While loop that compares the second and third images//////////////
         console.log('Images 2 and 3 were the same');
         var changepic = generateRandom(0, images.length);
         image2.src = ('./img/' + images[changepic].fileName);
-    }
+    };
 
-    image3.addEventListener('click', clickedPic)
+    image3.addEventListener('click', clickedPic);
     container.appendChild(image3);
     printedPics.push(image3);
-    
-
-    
-  
-
-    // console.log(sorted_pics);
 
 
-
-    // console.log(printedPics);
-
-    // console.log(sorted_pics[0].src);
-    // console.log(sorted_pics[1].src);
-    // console.log(sorted_pics[2].src);
-
-    // console.log(printedPics[0].currentSrc);
-    // console.log(printedPics[0].src);
-
-
+    while (image1 === image3) {
+        console.log('1 and 3 were the same!');
+        var changeEnds = generateRandom(0, images.length);
+        image3.src = ('./img/' + images[changeEnds].fileName);
+    }
 
 }
+
 
 
 
@@ -93,5 +79,7 @@ function generateRandom(min, max) {
 function clickedPic(event) {
         showThreeImages()
     }
+    
 
-    window.addEventListener('load', showThreeImages);
+
+window.addEventListener('load', showThreeImages);
