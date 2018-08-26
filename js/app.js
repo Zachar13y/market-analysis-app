989358e2b61f95ccc1a4ec97ef1f13fbb20bd9bc
Ido <idomskt@gmail.com>
2018-08-26 15:49:55 -0700

    Pretty much done, just need to generate a random number

--- js/app.js
+++ js/app.js
@@ -24,6 +24,7 @@ images.push(new BusMallImage('usb.jpg'));
 images.push(new BusMallImage('water_can.jpg'));
 images.push(new BusMallImage('wine_glass.jpg'));
 
+var rounds = 0;
 
 function getChartLabel(e) {
     var target = e.target.src;
@@ -38,6 +39,7 @@ function getChartLabel(e) {
     for(var i = 0; i < images.length; i++) {
         if(nameOfImg == images[i].fileName) { // ask == vs ===
             images[i].y++;
+            rounds++;
             chart.render();
         }
     }
@@ -52,45 +54,59 @@ function getChartLabel(e) {
     
     function showThreeImages() {
         var container = document.getElementById("img-container");
-        container.innerText = '';
-        var image1 = document.createElement('img');
-        var printedPics = [];
-        var index = generateRandom(0, images.length);
         
-        image1.setAttribute('src', './img/' + images[index].fileName);
-    
-        image1.addEventListener('click', getChartLabel);
-        container.appendChild(image1);
-        printedPics.push(image1);
+        if(rounds < 14) {
+            console.log(rounds);
+            container.innerText = '';
+            var printedPics = [];
+
+            var image1 = document.createElement('img');
+            var index = generateRandom(0, images.length);
+            
+            image1.setAttribute('src', './img/' + images[index].fileName);
         
-        var image2 = document.createElement('img');
-        index = generateRandom(0, images.length);
-        image2.setAttribute('src', './img/' + images[index].fileName);
-    
-        while (image1.src === image2.src) {   //////////While loop that compares the first two images//////////////
-            console.log('Images 1 and 2 were the same');
-            var changeIndex = generateRandom(0, images.length);
-            image2.src = ('./img/' + images[changeIndex].fileName);
-        };
-    
-        image2.addEventListener('click', getChartLabel);
-        container.appendChild(image2);
-        printedPics.push(image2);
+            image1.addEventListener('click', getChartLabel);
+            container.appendChild(image1);
+            printedPics.push(image1);
+            
+            var image2 = document.createElement('img');
+            index = generateRandom(0, images.length);
+            image2.setAttribute('src', './img/' + images[index].fileName);
         
+            while (image1.src === image2.src) {   //////////While loop that compares the first two images//////////////
+                console.log('Images 1 and 2 were the same');
+                var changeIndex = generateRandom(0, images.length);
+                image2.src = ('./img/' + images[changeIndex].fileName);
+                image3.src = ('./img/' + images[changeIndex].fileName);
+            };
         
-        image3 = document.createElement('img');
-        index = generateRandom(0, images.length);
-        image3.setAttribute('src', './img/' + images[index].fileName);
+            image2.addEventListener('click', getChartLabel);
+            container.appendChild(image2);
+            printedPics.push(image2);
+            
+            
+            image3 = document.createElement('img');
+            index = generateRandom(0, images.length);
+            image3.setAttribute('src', './img/' + images[index].fileName);
+            
+            while (image2.src === image3.src) { //////////While loop that compares the second and third images//////////////
+                console.log('Images 2 and 3 were the same');
+                var changepic = generateRandom(0, images.length);
+                image2.src = ('./img/' + images[changepic].fileName);
+            };
+            
+            image3.addEventListener('click', getChartLabel);
+            container.appendChild(image3);
+            printedPics.push(image3);
+
+        } else {
+            container.innerText = ' ';
+            var button = document.createElement('button');
+            container.appendChild(button);
+            button.setAttribute('onclick', 'location.reload()');
+            button.innerText = "Reset The Game";
+        }
         
-        while (image2.src === image3.src) { //////////While loop that compares the second and third images//////////////
-            console.log('Images 2 and 3 were the same');
-            var changepic = generateRandom(0, images.length);
-            image2.src = ('./img/' + images[changepic].fileName);
-        };
-        
-        image3.addEventListener('click', getChartLabel);
-        container.appendChild(image3);
-        printedPics.push(image3);
         
 
     
